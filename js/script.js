78bab3f662ab81812d4afc350c7b8358b6aef0b4
mad-lina <madelina.ecclesia@gmail.com>
2022-02-05 16:09:48 -0500

    display js variable

--- js/script.js
+++ js/script.js
@@ -2,17 +2,5 @@
 /*Pop Up*/
 alert('Hello world');
 
-let myName='Bob';
-document.write(myName);
-myName='John Doe';
-document.write(myName);
-
-let simpleAddition= 2+2;
-document.write(simpleAddition);
-
-let size=100;
-let doubleSize=100*2;
-document.write(doubleSize);
-
-let minSize=(doubleSize*2)-(size/2);
-document.write(minSize);
+let favoriteFood='Korean is my favorite food';
+document.write(favoriteFood);
