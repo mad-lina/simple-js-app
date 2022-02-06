1e4fac57e8568dae991da829cff8b16610128aa5
mad-lina <madelina.ecclesia@gmail.com>
2022-02-06 14:30:49 -0500

    2.2 Creates an array of Pokemon

--- js/script.js
+++ js/script.js
@@ -1,6 +1,13 @@
 
-/*Pop Up*/
-alert('Hello world');
+//three types of pokemon with three properties:name, height, types (an array of strings)
+let pokemonList=[
+  {name:'Venomoth', height:1.5, type:['Shield-grass', 'Tinted-lens']},
 
-let favoriteFood='Korean is my favorite food';
-document.write(favoriteFood);
+  {name:'Charizard', height:1.7, type:['Blaze', 'Solar-power']},
+
+  {name:'Nidoran', height:0.4, type:['Poison-point', 'Hustle']}
+];
+
+
+//check code accuracy
+document.write(pokemonList[2].type);
